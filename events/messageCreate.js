0e27e6a06cbe7707e244a221a4d1d11d01d81752
imconnorngl <connor.k.l3wis@gmail.com>
2021-09-07 11:08:28 +0100

    update framework to V13

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const Chalk = require('chalk');
+const moment = require('moment');
 
 const { prefix, developers } = require('../config/config.json');
 
@@ -27,6 +29,11 @@ module.exports = async (bot, message) => {
 
     if (command.access.length && !message.member.roles.cache.some(r => command.access.includes(r.id)) && !developers.includes(message.author.id) && !message.member.permissions.has("ADMINISTRATOR")) return message.error(`This command is locked to the following roles:\n\n${command.access.filter(a => message.guild.roles.cache.get(a)).map(a => `\`•\` ${message.guild.roles.cache.get(a).name}`).join("\n")}`)
     if (command.devOnly && !developers.includes(message.author.id)) return message.error(`This command requires you to be part of our development team.`);
+    
+    console.log(Chalk.blue(`[CMD] [${new moment().format('LTS')}] ${message.author.tag} in #${message.channel.name} used: ${message.content}`));
 
-    command.run(message, args, bot).catch(err => console.log(err));
+    command.run(message, args, bot).catch(err => {
+        console.log(err)
+        message.error(`An unexpected error has occured, please report this issue if it continues.\n\n\`\`\`diff\n- ${err.toString().split('\n')[0]}\n\`\`\``)
+    });
 }
--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const chalk = require('chalk')
+const Chalk = require('chalk')
 const moment = require('moment');
 
 /**
@@ -7,5 +7,5 @@ const moment = require('moment');
  * @param {Discord.Client} bot 
  */
 module.exports = (bot) => {
-    console.log(chalk.blueBright(chalk.bold(`Successfully launched ${bot.user.tag} with ${bot.guilds.cache.size || 0} guilds at ${new moment().format('LTS')}`)))
+    console.log(Chalk.blueBright(Chalk.bold(`Successfully launched ${bot.user.tag} with ${bot.guilds.cache.size || 0} guilds at ${new moment().format('LTS')}`)))
 }
